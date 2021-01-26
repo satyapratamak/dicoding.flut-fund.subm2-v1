70a70ff174e66a33153f506aa15d45d29d88e92c
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-01-27 02:17:12 +0700

    COMMIT : DONE!

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:restaurant_search_app_v1/model/mod_restaurant.dart';
 import 'package:restaurant_search_app_v1/style.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:dio/dio.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 
 void main() async {
   await DotEnv.load(fileName: '.env');
@@ -51,6 +52,8 @@ class _SearchPageState extends State<SearchPage> {
       '',
       queryParameters: {
         'q': query,
+        //'sort': 'rating',
+        //'order': 'desc',
       },
     );
     print(response);
@@ -63,8 +66,32 @@ class _SearchPageState extends State<SearchPage> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         backgroundColor: color_tart_orange,
+        actions: [
+          InkWell(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => SearchFilters(),
+                ),
+              );
+            },
+            child: Padding(
+              padding: const EdgeInsets.symmetric(
+                horizontal: 15,
+              ),
+              child: Icon(
+                Icons.tune,
+              ),
+            ),
+          ),
+        ],
       ),
       body: Center(
         child: Column(
@@ -134,13 +161,96 @@ class RestaurantItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(restaurant.name),
-      subtitle: Text(
-        restaurant.address,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 7),
+      child: Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        child: Row(
+          children: [
+            restaurant.thumbnail != null && restaurant.thumbnail.isNotEmpty
+                ? Ink(
+                    height: 100,
+                    width: 100,
+                    decoration: BoxDecoration(
+                      color: color_black_choclate,
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: NetworkImage(
+                          restaurant.thumbnail,
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(
+                    height: 100,
+                    width: 100,
+                    color: color_black_choclate,
+                    child: Icon(
+                      Icons.restaurant,
+                      size: 30,
+                      color: Colors.white,
+                    ),
+                  ),
+            Flexible(
+              child: Padding(
+                padding: const EdgeInsets.all(15),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      restaurant.name,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.location_on,
+                          color: color_orange_red,
+                          size: 15,
+                        ),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Flexible(
+                          child: Text(
+                            restaurant.locality,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    RatingBarIndicator(
+                      rating: double.parse(restaurant.rating),
+                      itemBuilder: (_, __) {
+                        return Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        );
+                      },
+                      itemSize: 20,
+                    ),
+                    Text(
+                      restaurant.rating,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
-      trailing:
-          Text('${restaurant.rating} stars, ${restaurant.reviews} reviews'),
     );
   }
 }
@@ -202,6 +312,8 @@ class _SearchFormState extends State<SearchForm> {
                       _autoValidateMode = AutovalidateMode.disabled;
                     });
                     widget.onSearch(_search);
+                    //Colapses keypad
+                    FocusManager.instance.primaryFocus.unfocus();
                   } else {
                     // TODO set autovalidate = true
                     setState(() {
@@ -227,6 +339,26 @@ class _SearchFormState extends State<SearchForm> {
             ),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class SearchFilters extends StatefulWidget {
+  @override
+  _SearchFiltersState createState() => _SearchFiltersState();
+}
+
+class _SearchFiltersState extends State<SearchFilters> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Filter your search'),
+        backgroundColor: color_tart_orange,
+      ),
+      body: Text(
+        'Filter your search',
       ),
     );
   }
