a5dac3a57be9cc75fad2d2782b53b8c6f50cf87b
DESKTOP-MR44V0P\Satya <satya0906@gmail.com>
2021-01-27 19:54:11 +0700

    COMMIT : Search & Details DONE!

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:restaurant_search_app_v1/style.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart' as DotEnv;
 import 'package:dio/dio.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
+import 'package:restaurant_search_app_v1/view/v_restaurant_detail.dart';
 
 void main() async {
   await DotEnv.load(fileName: '.env');
@@ -166,89 +167,100 @@ class RestaurantItem extends StatelessWidget {
       child: Card(
         clipBehavior: Clip.antiAlias,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
-        child: Row(
-          children: [
-            restaurant.thumbnail != null && restaurant.thumbnail.isNotEmpty
-                ? Ink(
-                    height: 100,
-                    width: 100,
-                    decoration: BoxDecoration(
-                      color: color_black_choclate,
-                      image: DecorationImage(
-                        fit: BoxFit.cover,
-                        image: NetworkImage(
-                          restaurant.thumbnail,
-                        ),
-                      ),
-                    ),
-                  )
-                : Container(
-                    height: 100,
-                    width: 100,
-                    color: color_black_choclate,
-                    child: Icon(
-                      Icons.restaurant,
-                      size: 30,
-                      color: Colors.white,
-                    ),
-                  ),
-            Flexible(
-              child: Padding(
-                padding: const EdgeInsets.all(15),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      restaurant.name,
-                      overflow: TextOverflow.ellipsis,
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    Row(
-                      children: [
-                        Icon(
-                          Icons.location_on,
-                          color: color_orange_red,
-                          size: 15,
-                        ),
-                        SizedBox(
-                          width: 5,
-                        ),
-                        Flexible(
-                          child: Text(
-                            restaurant.locality,
-                            overflow: TextOverflow.ellipsis,
+        child: InkWell(
+          onTap: () {
+            //final snackBar = SnackBar(content: Text(restaurant.name));
+            //Scaffold.of(context).showSnackBar(snackBar);
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) => RestaurantDetailScreen(restaurant)),
+            );
+          },
+          child: Row(
+            children: [
+              restaurant.thumbnail != null && restaurant.thumbnail.isNotEmpty
+                  ? Ink(
+                      height: 100,
+                      width: 100,
+                      decoration: BoxDecoration(
+                        color: color_black_choclate,
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                            restaurant.thumbnail,
                           ),
                         ),
-                      ],
+                      ),
+                    )
+                  : Container(
+                      height: 100,
+                      width: 100,
+                      color: color_black_choclate,
+                      child: Icon(
+                        Icons.restaurant,
+                        size: 30,
+                        color: Colors.white,
+                      ),
                     ),
-                    SizedBox(
-                      height: 8,
-                    ),
-                    RatingBarIndicator(
-                      rating: double.parse(restaurant.rating),
-                      itemBuilder: (_, __) {
-                        return Icon(
-                          Icons.star,
-                          color: Colors.amber,
-                        );
-                      },
-                      itemSize: 20,
-                    ),
-                    Text(
-                      restaurant.rating,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ],
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        restaurant.name,
+                        overflow: TextOverflow.ellipsis,
+                        style: TextStyle(
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      Row(
+                        children: [
+                          Icon(
+                            Icons.location_on,
+                            color: color_orange_red,
+                            size: 15,
+                          ),
+                          SizedBox(
+                            width: 5,
+                          ),
+                          Flexible(
+                            child: Text(
+                              restaurant.locality,
+                              overflow: TextOverflow.ellipsis,
+                            ),
+                          ),
+                        ],
+                      ),
+                      SizedBox(
+                        height: 8,
+                      ),
+                      RatingBarIndicator(
+                        rating: double.parse(restaurant.rating),
+                        itemBuilder: (_, __) {
+                          return Icon(
+                            Icons.star,
+                            color: Colors.amber,
+                          );
+                        },
+                        itemSize: 20,
+                      ),
+                      Text(
+                        restaurant.rating,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ],
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
@@ -332,6 +344,7 @@ class _SearchFormState extends State<SearchForm> {
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 18,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
                 ),
